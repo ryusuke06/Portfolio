e2629edbb3aa67c06d38c2db6de02b60d35ea7cf
vagrant <vagrant@localhost.localdomain>
2020-06-18 01:32:02 +0000

    お問い合わせ機能実装

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -3,8 +3,15 @@ class InquiriesController < ApplicationController
   	@inquiry = Inquiry.new
   end
 
+  def create
+    params[:inquiry] = {user_id: current_user.id, title: params[:title], content: params[:content], demand: params[:demand], read: params[:read]}
+    @inquiry = Inquiry.new(inquiries_params)
+    @inquiry.save
+    redirect_back(fallback_location: root_path)
+  end
+
   private
-  def inquiry_params
-  	params.require(:inquiry).permit(:title ,:content ,:demand ,:read)
+  def inquiries_params
+  	params.require(:inquiry).permit(:user_id ,:title ,:content ,:demand ,:read)
   end
 end
