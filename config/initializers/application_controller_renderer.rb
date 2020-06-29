833dc4d0371bea4d6c2c90043c922cf0484522fe
ryusuke06 <or.06.012@gmail.com>
2020-06-29 13:51:01 +0000

    refileのkey設定忘れてたわ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3aaf48c11bc9d6dc4c7c60be957a53349992db4c3465e35d8ffc5db6690a296f61435a71ef5c6e9981b3118b1db382d7aa2749490c627d0b4bc7283d66061f74'
